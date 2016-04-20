583cd5ea528a5e1523b58adf4340074888863596
Oleksandr Shturmov <oleks@oleks.info>
2016-04-20 12:57:06 +0200

    add celero as an external dependency

--- test/benchmark-datetime/main.cpp
+++ test/benchmark-datetime/main.cpp
@@ -1,7 +1,21 @@
 #include <realm.hpp>
+#include <celero/Celero.h>
+
+#include <random>
 
 using namespace realm;
 
-int main() {
+CELERO_MAIN
 
+std::random_device RandomDevice;
+std::uniform_int_distribution<int> UniformDistribution(0, 1024);
+
+BASELINE(DemoSimple, Baseline, 10, 1000000)
+{
+    celero::DoNotOptimizeAway(static_cast<float>(sin(UniformDistribution(RandomDevice))));
+}
+
+BENCHMARK(DemoSimple, Complex1, 1, 710000)
+{
+    celero::DoNotOptimizeAway(static_cast<float>(sin(fmod(UniformDistribution(RandomDevice), 3.14159265))));
 }
